98f9a9d76ea69a29b697aa22fab635d419b7f5b7
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-01-27 16:27:09 +0000

    added calculation lines

--- Module 2/M2T1.cpp
+++ Module 2/M2T1.cpp
@@ -1,6 +1,6 @@
 // CSC 134
 // M2T1
-// vinecal4701 (Brian V.)
+// vinecal4701 (B. Vineyard)
 // 1/27/2025
 
 #include <iostream>
@@ -12,11 +12,18 @@ int main()
 //This program will calculate and print a bill receipt
 
 //this will calculate the price before tax
+double price = 5.99;
+cout << "The price is $" << price << endl;
 
-//thi will calculate the amount of tax owed at 8%
+//this will calculate the amount of tax owed at 8%
+double tax = 0.08;
+cout << "The tax is " << tax << endl;
+double taxcalc = price * tax;
+cout << "The amount being added by the taxes is $" << taxcalc << endl;
 
 //this will print the total after taxes
-
+double total = price + taxcalc;
+cout << "Your total is $" << total << endl;
 
      return 0;
 }
