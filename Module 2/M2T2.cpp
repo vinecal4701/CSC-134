835a0d3c78030bdcde07d8db0770d7b43110c1aa
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-02-03 16:24:32 +0000

    removed prior version

--- Module 2/M2T2.cpp
+++ Module 2/M2T2.cpp
@@ -15,17 +15,17 @@ int main()
 
 //this will calculate the price before tax
 double price = 5.99;
-cout << "The price is $" << price << endl;
+cout << "The price is" << setw(12) << "$" << price << endl;
 
 //this will calculate the amount of tax owed at 8%
 double tax = 0.08;
 cout << "The tax is 8%." << endl;
 double taxcalc = price * tax;
-cout << "The amount being added by the taxes is $" << setprecision(2) << fixed << taxcalc << endl;
+cout << "Taxes added " << setw(12) << "$" << setprecision(2) << fixed << taxcalc << endl;
 
 //this will print the total after taxes
 double total = price + taxcalc;
-cout << "Your total is $" << total << endl;
+cout << "Your total is " << setw(10) << "$"  << total << endl;
 cout << endl;
 
      return 0;
