71b4f2fdf784c32e3f98f6f4545256595005a3da
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-02-10 16:56:58 +0000

    started HW

--- Module 2/M2HW1.cpp
+++ Module 2/M2HW1.cpp
@@ -1,16 +1,42 @@
 // CSC 134
-// M2HW1
+// M2HW1 - Gold + Bonus
 // vinecal4701 (Brian V.)
-// /2025
+// 2/10/2025
 
 #include <iostream>
-  
+#include <iomanip>
 using namespace std;
   
 int main()
 {
+// Question 1 start
 
+//Variables
+string first_name = "";
+string last_name = "";
+string name_on_account = first_name + "" + last_name;
+double starting_account_balance, deposit_amount,
+ withdrawl_amount, final_balance;
+int account_number
+int limitTo8Digits(int account_number);
 
+cout << fixed << setprecision(2);
+cout << "What is the name on the account?" << endl;
+cin >> first_name;
+cin >> last_name;
 
-     return 0;
+cout << "What is the starting account balance?\n";
+cin >> starting_account_balance;
+
+cout << "What is the amount you would like to deposit?" << endl;
+cin >> deposit_amount;
+
+cout << "What is the amount you would like to withdrawl?" << endl;
+cin >> withdrawl_amount;
+
+cout << name_on_account << endl;
+cout << account_number;
+final_balance = starting_account_balance + deposit_amount - withdrawl_amount;
+cout << final_balance
+return 0;
 }
