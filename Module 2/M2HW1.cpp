0aaac4b5043d750bb7654bf28603fb9faeba8fe6
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-02-16 03:26:35 +0000

    completed qurestion 1

--- Module 2/M2HW1.cpp
+++ Module 2/M2HW1.cpp
@@ -14,31 +14,39 @@ int main()
 //Variables
 string first_name = "";
 string last_name = "";
-string name_on_account = first_name + "" + last_name;
+
 double starting_account_balance, deposit_amount,
  withdrawl_amount, final_balance;
-int account_number;
-// figure out how to limit to 8 digits
-srand()
+int account_number = 25468739;
+
 
 cout << fixed << setprecision(2);
 cout << "What is the name on the account?" << endl;
 cin >> first_name;
 cin >> last_name;
-
+cout << endl;
 
 cout << "What is the starting account balance?\n";
+cout << "$";
 cin >> starting_account_balance;
+cout << endl;
 
 cout << "What is the amount you would like to deposit?" << endl;
+cout << "$";
 cin >> deposit_amount;
+cout << endl;
 
 cout << "What is the amount you would like to withdrawl?" << endl;
+cout << "$";
 cin >> withdrawl_amount;
+cout << endl;
 
-cout << name_on_account << endl;
-cout << account_number;
+string name_on_account = first_name + " " + last_name;
+cout << "The name on the account is: " << name_on_account << endl;
+cout << "Your account number is:     " << account_number << endl;
 final_balance = starting_account_balance + deposit_amount - withdrawl_amount;
-cout << final_balance;
+cout << "Your ending balance is:     " << final_balance << endl;
+cout << endl;
+
 return 0;
 }
