526aed56e9aafb597ed6e59d46adda9f43f66dd1
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-02-24 16:40:34 +0000

    finished M3T3

--- Module 3/M3T3.cpp
+++ Module 3/M3T3.cpp
@@ -32,23 +32,31 @@ cout << "Your roll was: " << roll2 << endl;
 total = roll1 + roll2;
 cout << "Your total is: " << total << endl;
 
+// Add our constants
+const int SNAKE_EYES = 2;
+const int THREE_LOSE = 3;
+const int BOX_CARS = 12;
+// WIN ON 7, 11
+const int LUCKY_SEVEN = 7;
+const int LUCKY_ELEVEN = 11;
+
 //Let's play Craps!
 // 7 and 12 -> win
 // 2, 3 and 12 -> lose
 // anything else -- comes later
-if (total == 7) {
+if (LUCKY_SEVEN == total) {
     cout << "Lucky seven! You win!" << endl;
 }
-else if (total == 11) {
+else if (LUCKY_ELEVEN == total) {
     cout << "Eleven is a winner!" << endl;
 }
-else if (total == 2) {
+else if (SNAKE_EYES == total) {
     cout << "Snake eyes! Too bad, you lose." << endl;
 }
-else if (total == 3) {
+else if (THREE_LOSE == total) {
     cout << "Sorry, three is unlucky. You lose." << endl;
 }
-else if (total == 12) {
+else if (BOX_CARS == total) {
     cout << "Boxcars! Sorry, you lost." << endl;
 }
 else {
