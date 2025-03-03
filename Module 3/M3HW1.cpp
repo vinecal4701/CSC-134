d5f9a0fb3650b8bf3b6fccdd15231b42fb63ab49
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-03-03 16:42:51 +0000

    started program

---
+++ Module 3/M3HW1.cpp
@@ -0,0 +1,38 @@
+// CSC 134
+// M3HW1
+// vinecal4701 (B. Vineyard)
+// 03/xx/2025
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+// beginning of the main() method
+
+    // this program will ask a question and respond to it with
+    // the program for the given question.
+
+    int choice;
+
+    cout << "What question's program would you like to run?" << endl;
+    cout << "Please type a number between 1 and 4: ";
+    cin >> choice;
+
+    
+
+
+
+
+
+
+cout << endl;
+cout << endl;
+cout << endl;
+
+return 0;
+}
