767164ed015935ef4ea63fe3fcd7e7a3525fe29b
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-03-03 16:49:48 +0000

    working on main function

--- Module 3/M3HW1.cpp
+++ Module 3/M3HW1.cpp
@@ -10,6 +10,11 @@
 
 using namespace std;
 
+void question_1();
+void question_2();
+void question_3();
+void question_4();
+
 int main()
 {
 // beginning of the main() method
@@ -23,7 +28,22 @@ int main()
     cout << "Please type a number between 1 and 4: ";
     cin >> choice;
 
-    
+    if (1 == choice){
+        question_1();
+    }
+    else if (2 == choice){
+        question_2();
+    }
+    else if (3 == choice){
+        question_3();
+    }
+    else if (4 == choice){
+        question_4();
+    }
+    else {
+        cout << "I'm sorry that is not a valid choice." << endl;
+        if ()
+    }
 
 
 
