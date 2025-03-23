59fec83c69485ac0afc21b15e84f50eacca9d230
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-03-23 16:19:30 +0000

    can't figure out why the loop doesn't circle back to ask if they want to quit after the conversion is printed

--- test.cpp
+++ test.cpp
@@ -1,32 +1,81 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
-{  
-   const double RATE = 5;
-   const double INITIAL_BALANCE = 10000;
-   const double TARGET = 2 * INITIAL_BALANCE;
+{
+   bool done = false;
+   string unit1 = "";
+   string unit2 = "";
+   double factor1 = 0; // conversion factor from first unit to cm
+   double factor2 = 0; // conversion factor from cm to second unit
    
-   cout << "Annual contribution: " << endl;
-   double contribution; 
-   cin >> contribution;
-
-   double balance = INITIAL_BALANCE;
-   int year = 0;
-
-   while (balance < TARGET){
-      year++;
-      //balance += contribution; // Add contribution at the beginning of the year
-      double interest = balance * RATE / 100;
-      balance += interest; // Add interest afterward
-      balance += contribution;
+   while (!done)
+   {
+      bool again = false; // true to repeat the same conversion
+      cout << "From unit (in, cm, m, again, quit): " << endl;
+      string command;
+      cin >> command;
+      if (command == "in")
+      {
+         factor1 = 2.54; 
+         unit1 = command;
+      }
+      else if (command == "cm")
+      {
+         factor1=1.0;
+         unit1 = command;
+      }
+      else if (command == "m")
+      {
+         factor1=100;
+         unit1 = command;/* Your code goes here */
+      }
+      else if (command == "again")
+      {
+         again = true;
+      }
+      else if (command == "quit")
+      {
+         done = true;
+      }
+      else
+      {
+         cout << "Sorry, unknown unit." << endl;
+      }
       
-      // Debugging: print balance after each year
-      cout << "Year: " << year << ", Balance after interest: " << balance << endl;
-   } 
-balance = balance - contribution;
-   cout << "Year: " << year << endl;
-   cout << "Final Balance: " << balance << endl;
+      if (factor1 != 0 && !again && !done)
+      {
+         cout << "To unit: " << endl;
+         cin >> unit2;
+         if (unit2 == "in")
+         {
+            factor2 = 1.0 / 2.54;
+         }
+         else if (unit2 == "m")
+         {
+            factor2 = 1.0/100.0;/* Your code goes here */
+         }
+         else if (unit2 == "cm")
+         {
+            factor2=1.0;
+         }
+         else
+         {
+            cout << "Sorry, unknown unit." << endl;
+         }
+      }
+      
+      if (unit2 == "in" || unit2== "cm" || unit2== "m") 
+      {
+         // Read value to be converted
+         double value;
+         cin >> value;
+         // Convert and print result
+         cout << value << " " << unit1 << " = " 
+            << value * factor1 * factor2 << " " << unit2 << endl;
+      }
+   }
 
    return 0;
 }
