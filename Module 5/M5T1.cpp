e53827d91c41a4c89c1709c9f10c76d52a5932ba
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-03-26 14:30:10 +0000

    added a void function and int function

--- Module 5/M5T1.cpp
+++ Module 5/M5T1.cpp
@@ -15,12 +15,20 @@ using namespace std;
 
 // Declare functions here, so main() knows they exist
 void show_message();
+void greet_user(string name);
+int double_a_number( int number);
 
 // main() here
 int main(){
     cout << "Hello from main()" << endl;
     show_message(); //call the function
-
+    string user = "Brian";
+    greet_user(user); //we're passing the value "Brian", not the actual variable
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+    int answer =double_a_number(num);
+    cout << "Double that is " << answer << endl;
 
 
 
@@ -37,4 +45,11 @@ int main(){
     // Define functions here, including full body {}
     void show_message(){
         cout << "This is a message" << endl;
+    }
+    void greet_user(string name){
+        cout << "Hello, " << name << endl;
+    }
+    int double_a_number(int number){
+        int answer =number * 2;
+        return answer; //must return an int
     }
