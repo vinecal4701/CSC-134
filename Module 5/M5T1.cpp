ca0ec2a96de62662eb4c8a96a04c543cacf80873
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-03-26 14:16:25 +0000

    started M5T1

---
+++ Module 5/M5T1.cpp
@@ -0,0 +1,40 @@
+// CSC 134
+// M5T1
+// vinecal4701 (B. Vineyard)
+// 03/26/2025
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+
+// Purpose: show how basic functions work
+
+// Declare functions here, so main() knows they exist
+void show_message();
+
+// main() here
+int main(){
+    cout << "Hello from main()" << endl;
+    show_message(); //call the function
+
+
+
+
+
+
+
+//done
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    return 0;
+    }
+
+    // Define functions here, including full body {}
+    void show_message(){
+        cout << "This is a message" << endl;
+    }
