939bd36ffcb70e3171287ad8dee6490a39bb71b3
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-03-30 02:52:29 +0000

    tweaking arguements

--- Module 4/M4HW1.cpp
+++ Module 4/M4HW1.cpp
@@ -18,20 +18,17 @@ int choice_routine(int choice);
 int product_calc( int number1, int number2); // does the math
 
 int main(){
-   // cout << "Hello from main()" << endl;
-    //show_message(); //call the function
-    //square_a_number_routine();
     cout << "Hello! This program will calculate and display the product table of a number." << endl;
     bool is_valid = false;
     while (is_valid == false){
     int choice;
-    //show_message();
     cin >> choice;
-    int selection = choice;
-    //choice_routine(selection);
+        
     if (choice >= 1 && choice <= 12){
-        cout << choice;
-        product_of_a_number_routine(choice);
+        int selection = choice;
+        //cout << choice;
+        choice_routine(selection);
+        product_of_a_number_routine();
         is_valid = true;
     }
     else {
@@ -47,7 +44,7 @@ int main(){
     cout << endl;
     return 0;
     }
-/*
+
     // Define functions here, including full body {}
     void show_message(){
         cout << "You may choose any number between 1 and 12." << endl;
@@ -55,25 +52,24 @@ int main(){
 
 
     int choice_routine(int choice){
-        int selection;
-        cin >> selection;
-return selection;
+        int selection = choice;
+    return selection;
     }
 
 
     void product_of_a_number_routine(){
-        int answer;
+        int selection = choice_routine();
         const int MIN_NUM = 1;
         const int MAX_NUM = 60;
         cout << "Table of Products" << endl;
         cout << "--------------------" << endl;
         int num = MIN_NUM;
         while (num <= MAX_NUM){
-            answer = product_calc();
+            int answer = product_calc(selection,num);
             cout << choice_routine() << "*" << num << "= " << answer << endl;
             /* "\t" adds a space by "hitting" tab.
             Can be repeated back to back as long as data doesn't spill over to next line*/
-           /* num = num + 1;
+           num = num + 1;
         }
     }
 
@@ -85,4 +81,4 @@ return selection;
     }
 
 
-    */
+    
