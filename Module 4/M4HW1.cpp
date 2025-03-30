463399abed83b2876566221260da710df98d32f1
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-03-30 03:14:43 +0000

    got it working and outputting right

--- Module 4/M4HW1.cpp
+++ Module 4/M4HW1.cpp
@@ -16,13 +16,19 @@ void show_message();
 void product_of_a_number_routine(); // does everything but the math
 int choice_routine(int choice);
 int product_calc( int number1, int number2); // does the math
-
+int choice;   /* Had to ask Microsoft Copilot 
+where to place this line as I was running into errors where
+ the different functions weren't receiving the input for it. */
 int main(){
     cout << "Hello! This program will calculate and display the product table of a number." << endl;
+    //int choice;
+    show_message();   /* Had to ask Microsoft Copilot 
+    where to place this line as I was running into errors where
+    the different functions weren't receiving the input for it. */
+    
     bool is_valid = false;
     while (is_valid == false){
-    int choice;
-    cin >> choice;
+    
         
     if (choice >= 1 && choice <= 12){
         int selection = choice;
@@ -33,6 +39,7 @@ int main(){
     }
     else {
         cout << "Sorry, not valid. Try again." << endl;
+        show_message();
     }
     cout << "Goodbye" << endl;
     }
@@ -47,7 +54,10 @@ int main(){
 
     // Define functions here, including full body {}
     void show_message(){
-        cout << "You may choose any number between 1 and 12." << endl;
+        cout << "You may choose any number between 1 and 12. Type 0 to quit." << endl;
+        cin >> choice;   /* Had to ask Microsoft Copilot 
+        where to place this line as I was running into errors where
+        the different functions weren't receiving the input for it. */
     }
 
 
@@ -58,15 +68,15 @@ int main(){
 
 
     void product_of_a_number_routine(){
-        int selection = choice_routine();
+        int selection = choice_routine(choice);
         const int MIN_NUM = 1;
-        const int MAX_NUM = 60;
+        const int MAX_NUM = 12;
         cout << "Table of Products" << endl;
         cout << "--------------------" << endl;
         int num = MIN_NUM;
         while (num <= MAX_NUM){
             int answer = product_calc(selection,num);
-            cout << choice_routine() << "*" << num << "= " << answer << endl;
+            cout << choice_routine(choice) << "*" << num << "= " << answer << endl;
             /* "\t" adds a space by "hitting" tab.
             Can be repeated back to back as long as data doesn't spill over to next line*/
            num = num + 1;
