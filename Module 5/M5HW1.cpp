8ccba4e24462e098ddb2129cfca7eb4a0c6fa861
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-04-07 14:37:51 +0000

    created for assignments

---
+++ Module 5/M5HW1.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M5HW1
+// vinecal4701 (B. Vineyard)
+// 4/xx/2025
+
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    
+    
+    
+    
+    
+    
+    
+    cout << endl;
+    
+    return 0;
+}
