e032cc934c5d0bfaee6f1f5a2e9fb628812e7550
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-04-21 15:04:19 +0000

    DONE

--- Module 6/M6LAB1.cpp
+++ Module 6/M6LAB1.cpp
@@ -34,9 +34,27 @@ int main()
         cout << "Next shell: " << shellname << endl;
     }
 
-    
-    
-    
+    //Example two: using a vector
+    // .size() tells you how many items are in the vector
+    // .push_back() adds an item to the back (end) of the vector
+    // .pop_back() removes an item from the back (end) of the vector
+    vector<int> mag;
+    //load mag
+    mag.push_back(BLACK);
+    mag.push_back(RED);
+    int num_shells = 2;
+    // look at shell before you eject it
+    int current_shell = mag.at(num_shells-1);
+    cout << "Next shell: " << current_shell << endl;
+    //eject a shell
+    mag.pop_back(); //this removes the last shell
+    num_shells--;
+    //repeat
+    current_shell = mag.at(num_shells-1);
+    cout << "Next shell: " << current_shell << endl;
+    mag.pop_back(); // removes last shell
+    cout << "Mag contains " << mag.size() << " shells." << endl;
+    cout << "It could hold up to " << mag.capacity() << " before resizing." << endl;
     
     
     cout << endl;
