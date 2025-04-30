b48603a820b4860ba7542dd05a61e211865af4d0
vinecal4701 <vineyarb4701@student.faytechcc.edu>
2025-04-30 14:55:32 +0000

    test

---
+++ Module 7/M7T2.cpp
@@ -0,0 +1,88 @@
+// CSC 134
+// M7T2
+// vinecal4701 (B. Vineyard)
+// 4/30/2025
+
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Rectangle {
+    private:
+        double width;
+        double length;
+    public:
+        void setWidth(double w);
+        void setLength(double len);
+        // get() should never change anything, so const makes sure we don't accidentally change something
+        double getWidth() const;
+        double getLength() const;
+        double getArea() const;
+};
+
+// implementation of class functions
+void Rectangle::setWidth(double w){
+    width - w;
+}
+
+void Rectangle::setLength(double len){
+    length = len;
+}
+
+double Rectangle::getWidth() const{
+    return width;
+}
+
+double Rectangle::getLength() const{
+    return length;
+}
+
+double Rectangle::getArea() const{
+    double area = width * length;
+    return area;
+}
+
+
+// main goes here
+int main()
+{    
+
+    double len, wid, area;
+    cout << "Rectangle Calculations" << endl;
+    cout << "Rectangle 1" << endl;
+    cout << "enter width ";
+    do{
+        cin >> wid;
+        if(wid <= 0){
+        cout << "Width can not be negative. Please enter a postive value." << endl;
+        cin.ignore();
+        }
+    }while (wid <= 0);
+    cout << "enter length: ";
+    do{
+        cin >> len;
+        if(len <= 0){
+        cout << "Length can not be negative. Please enter a postive value." << endl;
+        cin.ignore();
+        }
+    }while (len <= 0);
+        
+
+    Rectangle r1 = Rectangle();
+    r1.setWidth(wid);
+    r1.setLength(len);
+
+    cout << "The area is: " << r1.getArea() << endl;
+
+
+
+
+    
+    cout << endl;
+    
+    return 0;
+}
